fee019de68eeab8d8cd45aed7c78c1eb385248d1
joubertjose <joubertjose200614@gmail.com>
2024-10-04 16:08:30 +0530

    Add files via upload

---
+++ tadow.py
@@ -0,0 +1,53 @@
+'''
+print("hello, world")
+print("hello\nworld")
+if 5>2:
+    print("five is greater than two!")
+    print("five is greater than two!")
+'''
+
+'''
+x =5
+x='thomas'
+print(x)
+'''
+'''
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x,y,z)
+'''
+'''
+x = 1j
+y = "john"
+print(type(x))
+print(type(y))
+'''
+'''
+x, y, z = "ornge", "banana","cherry"
+print(x)
+print(y)
+print(z)
+'''
+'''
+x = y = z = 'orange'
+print(x)
+print(y)
+print(z)
+'''
+'''
+x = "python"
+y = "is"
+z = "awesome"
+print(x + y + z)
+'''
+'''
+username = input("enter username:")
+print("username is: " + username)
+'''
+a = "hello"
+b = "world"
+c = a+b
+print(c)
+
