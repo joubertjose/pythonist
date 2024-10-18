5f98cf3e6a1ea31feb81efb2bd1d3c862faa200e
joubertjose <joubertjose200614@gmail.com>
2024-10-18 15:58:53 +0530

    Add files via upload

---
+++ program 3.py
@@ -0,0 +1,7 @@
+number1 =int(input("Enter the first number:"))
+number2 =int(input("Enter the second number:"))
+print("sum=",number1+number2,",Division=",number1/number2)
+print("Is number1 greater than number2?:",number1>number2)
+print ("Are number1 and number2 equal?:",number1==number2)
+print("Logical AND:",number1>0 and number2>0)
+print("Logical OR:",number1>0 or number2>number1)
