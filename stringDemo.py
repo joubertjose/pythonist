9a90b6ddf0f09c0c9dcfe9b75fb139c146e1f3cf
joubertjose <joubertjose200614@gmail.com>
2024-10-18 16:14:45 +0530

    Update stringDemo.py

--- stringDemo.py
+++ stringDemo.py
@@ -1,5 +1,5 @@
 '''
-Author: nigga
+Author: joubert jose
 Date: 18 - 10 - 2024
 Description:program that stores a string in a variable.
 Extract a specific part of the string (substring) and then concatenate
