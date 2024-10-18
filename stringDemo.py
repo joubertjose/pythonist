7f075dfc0a6a730eddb26ff06723d324038f5d4c
joubertjose <joubertjose200614@gmail.com>
2024-10-18 15:08:18 +0530

    Add files via upload

---
+++ stringDemo.py
@@ -0,0 +1,11 @@
+'''
+Author: nigga
+Date: 18 - 10 - 2024
+Description:program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate
+it with another string. Finally, display the new string.
+'''
+str_var1=("Hello, World!")
+str_var2=str_var1[7:12]
+str_var3=" Everyone!"
+print(str_var2+str_var3)
