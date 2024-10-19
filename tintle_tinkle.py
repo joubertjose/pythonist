4f673bf06d5cda8282490f5dd5632a5a603fbfa8
joubertjose <joubertjose200614@gmail.com>
2024-10-19 15:44:17 +0530

    Add files via upload

---
+++ tintle_tinkle.py
@@ -0,0 +1,15 @@
+'''
+Author:Joubert jose
+Date:19-10-2024
+Desctription:program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+'''
+print("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
