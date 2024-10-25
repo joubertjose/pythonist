227d22d5bb8c4919ece562f9bfda28fd54b9c8bd
joubertjose <joubertjose200614@gmail.com>
2024-10-25 14:39:32 +0530

    Add files via upload

---
+++ condition2.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the first number"))
+if num1>0:
+    print(num1,"is a positive number",0)
+elif num1<0:
+    print(num1,"is a negative number")
+else:
+    print(num1,"is a zero",0)
