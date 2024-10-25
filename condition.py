f01e1c535f2ff7e65bfd028c99331bd4aab7825f
joubertjose <joubertjose200614@gmail.com>
2024-10-25 14:26:08 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,11 @@
+'''Author:Bichiu Sasi
+Date:19-10-2024
+Discription:Python program to find the largest of three numbers. The program should take three numbers as
+input from the user and determine which one is the largest. '''
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+if num1>num2:
+    print(num1,"is greater than",num2)
+else:
+    print(num2,"is greater than ",num1)
+print("The remaining statement")
