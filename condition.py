367e00f92dbbae03a23f997711cddfc837a734ff
joubertjose <joubertjose200614@gmail.com>
2024-10-25 14:46:38 +0530

    Update condition.py

--- condition.py
+++ condition.py
@@ -4,7 +4,7 @@ Discription:Python program to find the largest of three numbers. The program sho
 from the user and determine which one is the largest. '''
 num1=int(input("Enter the first number"))
 num2=int(input("Enter the second number"))
-num3=int(input("enter the third number"))
+num3=int(input("Enter the third number"))
 if num1>num2:
     print(num1,"is greater than",num2)
 else:
