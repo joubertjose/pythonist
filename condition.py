44408fb1b8a5f57b1e11f33196e1fcfe6757a0fa
joubertjose <joubertjose200614@gmail.com>
2024-10-25 14:53:17 +0530

    Update condition.py

--- condition.py
+++ condition.py
@@ -5,8 +5,9 @@ from the user and determine which one is the largest. '''
 num1=int(input("Enter the first number"))
 num2=int(input("Enter the second number"))
 num3=int(input("Enter the third number"))
-if num1>num2:
-    print(num1,"is greater than",num2)
+if num1>num2 and num1>num3:
+    print(num1,"is greater than the other two")
+elif num2>num3:
+    print(num2,"is greater than the other two")
 else:
-    print(num2,"is greater than ",num1)
-print("The remaining statement")
+    print(num3,"is greater than the other two")
