9000488836b1523d0b1568eae747010f5fb66505
joubertjose <joubertjose200614@gmail.com>
2024-10-28 10:53:05 +0530

    Add files via upload

---
+++ temp_converter.py
@@ -0,0 +1,15 @@
+
+while True:
+    print("1.\tConvert Celsius to Fahrenheit")
+    print("2.\tConvert Fahrenheit to Celsius")
+    print("3.\tExit the program")
+    choice=int(input("Enter your choice:"))
+    temperature=int(input("Enter your temperature"))
+    if choice==1:
+        temperature_in_f=(9/5*(temperature)+32)
+        print(f"temperature in celcius"=temperature_in celcius)
+
+
+
+
+
